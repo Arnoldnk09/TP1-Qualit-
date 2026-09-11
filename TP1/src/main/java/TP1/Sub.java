b7e1e6875283bb0edec7be2ee72262e07e641567
Etudiant <Etudiant@INFO-B009-PC16.crie.i-univ-tlse2.fr>
2026-09-11 13:56:22 +0200

    Sub

---
+++ TP1/src/main/java/TP1/Sub.java
@@ -0,0 +1,8 @@
+package TP1;
+
+
+public class Sub {
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
---
+++ TP1/src/test/java/TP1/SubTest.java
@@ -0,0 +1,17 @@
+package TP1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class SubTest {
+
+    /**
+     * Test
+     */
+    @Test
+    public void TestSub() {
+        assertEquals(2, Sub.sub(5, 3));
+    }
+}
